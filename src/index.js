e6a2a745f5a42182b583f849ba16625104df632b
pallabiram <kumaripallavi5539@gmail.com>
2022-12-15 09:55:10 +0530

    Assinment completed

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route = require('./route/routes.js');
+const mongoose = require('mongoose');
+const app = express();
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+mongoose.connect("mongodb+srv://jay420:gRLzeLdOa6ENyasF@cluster0.dnkg3q6.mongodb.net/assignment", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route)
+
+app.use("/*", function (req, res) {
+    return res.status(400).send({
+        status: false, message: "Make Sure Your Endpoint is Correct !!!"
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
